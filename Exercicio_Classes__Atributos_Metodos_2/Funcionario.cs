04467aef301c185a321a89ad996aa3e7d66cceb3
Rubem Silva <rubem.edu@outlook.com>
2021-02-28 15:01:59 -0300

    Add project files.

---
+++ Exercicio_Classes__Atributos_Metodos_2/Funcionario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio_Classes__Atributos_Metodos_2
+{
+    class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido()
+        {
+            
+            return (SalarioBruto-Imposto);
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            double percentual = porcentagem/100.0;
+            double p = SalarioBruto * percentual;
+            SalarioBruto += p;
+            SalarioBruto -= Imposto;
+
+        }
+    }
+}
---
+++ Exercicio_Classes__Atributos_Metodos_2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+namespace Exercicio_Classes__Atributos_Metodos_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario = new Funcionario();
+
+            Console.Write("Nome : ");
+            funcionario.Nome = Console.ReadLine();
+
+            Console.Write("Salario Bruto : ");
+            funcionario.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Imposto : ");
+            funcionario.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+           
+            Console.Write("Funcionário : "+funcionario.Nome+", $ "+funcionario.SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine();
+            Console.Write("Digite a porcentagem para aumentar o salário : ");
+            double porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            funcionario.AumentarSalario(porcentagem);
+           
+            Console.Write("Dados Atualizados : "+funcionario.Nome+", $ "+funcionario.SalarioBruto.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine();
+
+        }
+    }
+}
